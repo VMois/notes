67045024101a7d347892e21380ab008c69309782
vmois <vladyslav.moisieienkov@gmail.com>
2018-08-02 20:44:33 +0200

    d3: Add vertical bar

--- data-science/visualization/d3js/tutorials/simple-bar-chart/index.js
+++ data-science/visualization/d3js/tutorials/simple-bar-chart/index.js
@@ -1,9 +1,15 @@
-const data = [30, 86, 168, 281, 303, 365];
+const data = [2, 14, 7, 32, 25, 42];
+
+console.log(d3);
+
+const x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
 
 d3.select(".chart")
 .selectAll("div")
 .data(data)
 .enter()
 .append("div")
-.style("width", (d) => d + "px")
+.style("height", (d) => x(d) + "px")
 .text((d) => d)
