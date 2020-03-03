5bfc85349ca37a781ebb72b649c3827453eebbb8
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2020-03-03 19:26:31 +0100

    Add variable_types_size.cpp for C++

--- programming/c++/variable_types_size.cpp
+++ programming/c++/variable_types_size.cpp
@@ -1,7 +1,8 @@
-#include <iostream>  // preprocessor with brackets
+#include <iostream> 
 
 using namespace std;
-int main()
+
+int main() 
 {
     cout << "Variable type size in bytes:" << endl;
     cout << "Int: " << sizeof(int) << endl;
